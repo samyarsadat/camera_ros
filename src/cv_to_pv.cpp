f2fcff302a3830095437f8a57190fe475dc628a9
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2025-01-23 20:16:30 +0300

    Modified debug logging.

--- src/cv_to_pv.cpp
+++ src/cv_to_pv.cpp
@@ -142,7 +142,7 @@ cv_to_pv(const libcamera::ControlValue &value)
 rclcpp::ParameterType
 cv_to_pv_type(const libcamera::ControlId *const id)
 {
-  RCLCPP_INFO_STREAM(get_logger(), "get_extent() for " << id << " is " << get_extent(id));
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("camera"), "get_extent() for " << id << " is " << get_extent(id) << ", id->type() is " << id->type());
   if (get_extent(id) == 0) {
     switch (id->type()) {
     case libcamera::ControlType::ControlTypeNone:
@@ -200,6 +200,6 @@ cv_to_pv_type(const libcamera::ControlId *const id)
     }
   }
 
-  RCLCPP_INFO_STREAM(get_logger(), "End of cv_to_pv_type()");
+  RCLCPP_INFO_STREAM(rclcpp::get_logger("camera"), "End of cv_to_pv_type()");
   return rclcpp::ParameterType::PARAMETER_NOT_SET;
 }
