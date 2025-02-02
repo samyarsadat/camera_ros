d40d1b14bcb592950f72364409d86dac5657cccb
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2025-02-02 21:52:28 +0300

    Minor re-formatting and cleanup.

--- src/CameraNode.cpp
+++ src/CameraNode.cpp
@@ -225,17 +225,6 @@ CameraNode::CameraNode(const rclcpp::NodeOptions &options) : Node("camera", opti
   param_descr_ci_file_url.read_only = true;
   declare_parameter<std::string>("ci_file_url", {}, param_descr_ci_file_url);
 
-  std::string ci_file_url_param = get_parameter("ci_file_url").as_string();
-  if (!ci_file_url_param.empty()) {
-    if (cim.validateURL(ci_file_url_param)) {
-      RCLCPP_INFO_STREAM(get_logger(), "loading camera calibration info from: \"" << ci_file_url_param << "\"");
-      cim.loadCameraInfo(ci_file_url_param);
-    }
-    else {
-      throw std::runtime_error("invalid camera info file path");
-    }
-  }
-
   // camera ID
   declare_parameter("camera", rclcpp::ParameterValue {}, param_descr_ro.set__dynamic_typing(true));
 
@@ -407,6 +396,17 @@ CameraNode::CameraNode(const rclcpp::NodeOptions &options) : Node("camera", opti
     cname.cend());
   std::replace_if(
     cname.begin(), cname.end(), [](const char &x) { return !std::isalnum(x); }, '_');
+
+  std::string ci_file_url_param = get_parameter("ci_file_url").as_string();
+  if (!ci_file_url_param.empty()) {
+    if (cim.validateURL(ci_file_url_param)) {
+      RCLCPP_INFO_STREAM(get_logger(), "loading camera calibration info from: \"" << ci_file_url_param << "\"");
+      cim.loadCameraInfo(ci_file_url_param);
+    }
+    else {
+      throw std::runtime_error("invalid camera calibration info file url");
+    }
+  }
 
   if (!cim.setCameraName(cname))
     throw std::runtime_error("camera name must only contain alphanumeric characters");
